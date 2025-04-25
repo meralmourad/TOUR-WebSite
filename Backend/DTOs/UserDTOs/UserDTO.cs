c4660e0437e4266f3c6e3393756b54c11c5359f0
= <=>
2025-04-25 23:17:09 +0300

    add validations to dtos

--- Backend/DTOs/UserDTOs/UserDTO.cs
+++ Backend/DTOs/UserDTOs/UserDTO.cs
@@ -1,11 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Backend.DTOs.UserDTOs;
 
 public class UserDTO
 {
     public required int Id { get; init; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
     public required string Name { get; init; }
+
+    [Required]
+    [EmailAddress]
     public required string Email { get; init; }
+
+    [Required]
     public required string Role { get; init; }
+
+    [Phone]
     public string? PhoneNumber { get; init; }
+
+    [StringLength(200)]
     public string? Address { get; init; }
 }
