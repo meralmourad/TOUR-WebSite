e7af7346d2b65aa2fb11de304c0d5a20c976409a
HanyMaged4 <hany.maged9102@gmail.com>
2025-05-11 00:44:37 +0300

    sorry fady

--- Backend/Controllers/UserController.cs
+++ Backend/Controllers/UserController.cs
@@ -69,6 +69,7 @@ namespace Backend.Controllers
                    Name = retuser.Name,
                     Email = retuser.Email,
                     PhoneNumber = retuser.PhoneNumber,
+                    Address = retuser.Address,
                     Role = retuser.Role,
                 };
                 return Ok(userDto);
