d1b25fbe91a89bc9ff85800be9b1e8433680eb54
HanyMaged4 <hany.maged9102@gmail.com>
2025-05-11 00:48:08 +0300

    sorry fady2

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2e03b1ca4f12f5e003bb6385bc50f5b75ca4c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7af7346d2b65aa2fb11de304c0d5a20c976409a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
