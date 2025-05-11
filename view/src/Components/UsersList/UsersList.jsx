ad251b77d21f3c1205f1cda4ae3679907a664c0c
Mechan <meral.mourad.24@gmail.com>
2025-05-12 02:40:09 +0300

    .

--- view/src/Components/UsersList/UsersList.jsx
+++ view/src/Components/UsersList/UsersList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./UsersList.scss";
 import { useNavigate } from "react-router-dom";
 import { SearchUsers } from "../../service/UserService";
+import { shallowEqual } from "react-redux";
 
 const numberOfUsersPerPage = 9;
 
