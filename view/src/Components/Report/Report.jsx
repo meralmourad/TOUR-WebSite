195f9e795296a2ce0d499a3000e6de1c339ab18b
Fady751 <Fady.soly55@gmail.com>
2025-05-14 07:53:01 +0300

    a3333333333333333

--- view/src/Components/Report/Report.jsx
+++ view/src/Components/Report/Report.jsx
@@ -1,29 +1,34 @@
 import { useEffect, useState } from 'react';
 import './Report.scss';
-import { useParams } from 'react-router-dom';
+import swal from "sweetalert";
+import { useNavigate, useParams } from 'react-router-dom';
 import { getReportsByTripId } from '../../service/ReportService';
 
 const Report = () => {
     const id = useParams().id;
     const [reports, setReports] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchReport = async () => {
             try {
                 const reports = await getReportsByTripId(id);
                 setReports(reports);
+                // console.log(reports);
             } catch (error) {
                 console.error('Error fetching report:', error);
+                swal("Opps!", error.response.data, "error");
+                navigate(`/Trip/${id}`);
             }
         };
 
         fetchReport();
-    }, [id]);
+    }, [id, reports, navigate]);
 
     return (
         <div className="report-container">
             {reports.map((report) => (
-                <div key={reports.id} className="report-card">
+                <div key={reports} className="report-card">
                     <p>{report.content}</p>
                 </div>
             ))}
