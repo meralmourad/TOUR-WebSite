afa7b1881557f462136878c0bb48478f20da32f3
Mechan <meral.mourad.24@gmail.com>
2025-05-14 10:06:02 +0300

    f**king rate & report

--- view/src/service/ReportService.jsx
+++ view/src/service/ReportService.jsx
@@ -1,22 +1,35 @@
-import axios from 'axios';
-import { getUserById } from './UserService';
+import axios from "axios";
+import { getUserById } from "./UserService";
 
 const API_URL = process.env.REACT_APP_API_URL;
 const token = JSON.parse(localStorage.getItem("Token"))?.token;
 
 export const getReportsByTripId = async (tripId) => {
-    try {
-        const response = await axios.get(`${API_URL}/Report/trip/${tripId}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            }
-        });
-        const data = response.data.$values;
-        for (let i = 0; i < data.length; i++) {
-            data[i].sender = await getUserById(data[i].senderId);
-        }
-        return data;
-    } catch (error) {
-        throw error;
+  try {
+    const response = await axios.get(`${API_URL}/Report/trip/${tripId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = response.data.$values;
+    for (let i = 0; i < data.length; i++) {
+      data[i].sender = await getUserById(data[i].senderId);
     }
-}
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const sendReport = async (report) => {
+  try {
+    const response = await axios.post(`${API_URL}/Report`, report, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    });
+    return response ;
+  } catch (error) {
+    throw error;
+  }
+};
